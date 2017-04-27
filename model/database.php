29265bda446bf7bc836ec62aeb8fb1f7e7a01bf6
Rebecca J Cortes <rjc37@afsconnect2.njit.edu>
2017-04-27 02:45:26 -0400

    updated

--- model/database.php
+++ model/database.php
@@ -1,7 +1,7 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=my_guitar_shop1';
-    $username = 'mgs_user';
-    $password = 'pa55word';
+    $dsn = 'mysql:host=sql2.njit.edu;dbname=rjc37';
+    $username = 'rjc37';
+    $password = 'cosette2';
 
     try {
         $db = new PDO($dsn, $username, $password);
